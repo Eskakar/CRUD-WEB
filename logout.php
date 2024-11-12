4433a42b246a5d1bdd96da5f9c705fe5286ab8ba
Eskakar <150883108+Eskakar@users.noreply.github.com>
2024-11-12 18:17:27 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();          // mengaktifkan session
 session_destroy();       // menghapus semua session
- 
+ //<!--123230085-->
 // mengalihkan halaman sambil mengirim pesan logout
 header("location:index.php?pesan=logout");
 ?>
